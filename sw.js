2d7fc3f02785d09a8fd73cab0f4bd4f1d9262dbd
hakatax-cmd <hakata.x@gmail.com>
2026-06-23 21:48:21 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'ohayo-ready-v1'
+const asset = (path) => new URL(path, self.location.href).href
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'].map(asset)
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)))
+  self.skipWaiting()
+})
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))))
+  self.clients.claim()
+})
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(fetch(event.request).then((response) => {
+    const copy = response.clone()
+    caches.open(CACHE).then((cache) => cache.put(event.request, copy))
+    return response
+  }).catch(() => caches.match(event.request).then((cached) => cached || caches.match(asset('./index.html')))))
+})
